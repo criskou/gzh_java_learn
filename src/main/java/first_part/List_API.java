e310a66543ac6475e8aca57bf65e9da21429628d
gzh <gzh@qq.com>
2022-10-21 11:19:20 +0800

    2022/10/20 LinkedList方法调用问题-遍历集合方式commit

--- src/main/java/first_part/List_API.java
+++ src/main/java/first_part/List_API.java
@@ -18,6 +18,9 @@ public class List_API {
         //问题拓展：常见集合的常用方法有哪些？
         //Java List集合全解
         //https://blog.csdn.net/a1781842634/article/details/116516967?ops_request_misc=%257B%2522request%255Fid%2522%253A%2522166627641616782425167767%2522%252C%2522scm%2522%253A%252220140713.130102334..%2522%257D&request_id=166627641616782425167767&biz_id=0&utm_medium=distribute.pc_search_result.none-task-blog-2~all~baidu_landing_v2~default-5-116516967-null-null.142^v59^opensearch_v2,201^v3^control_1&utm_term=java%20List&spm=1018.2226.3001.4187
+
+        //遍历集合的方式
+        //foreach\迭代器
     }
 
     @Test
